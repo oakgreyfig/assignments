91057a4c876eb0448f2f7a5eb597baff5325acb6
John Whalen <john@grcand.me>
2022-10-24 15:16:20 -0700

    calc first function

---
+++ JS-DOM-Environment/calculator/calc.js
@@ -0,0 +1,11 @@
+const readline = require("readline-sync")
+
+function add (x,y) {
+    return x+y;
+}
+console.log(add(2, 3))
+console.log(add(-1, 1))
+console.log(add(1, 1, 1)) 
+
+// shows that the third input doesn't have anything to the equatuion. output is 2 not 3// 
+
