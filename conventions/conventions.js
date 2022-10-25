2072c7e7e9dd8637878ea941677eb9b33480f903
John Whalen <john@grcand.me>
2022-10-24 20:13:53 -0700

    working on new and better notes to reference in vsc

---
+++ conventions/conventions.js
@@ -0,0 +1,44 @@
+// JS Conventions
+
+
+// casing camelCase 
+
+var lastName
+var lName 
+var myLocationIsValide = true 
+
+// white space doesn't read whitespace makes it easier to read 
+
+
+for (var i = 0;i< 10;i++) {
+
+}
+// naming name a variable what it is 
+
+var fN = "firstname "
+var firstName = "makes more sense"
+
+function sum (num1,num2){
+
+}
+
+var friends = []
+var isDone = true
+var isGameOver = false
+
+
+// braces how top spot opening and closing pairs 
+
+var person = {
+    address: {
+        
+    }
+}
+
+// tab (indenting)
+
+// HTML tags
+
+// comments
+
+// unix casing
